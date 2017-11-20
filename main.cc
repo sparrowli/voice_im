1b1e8416fa436185441ca5e5e76989ee99250b2a
sparrow <280669537@qq.com>
2017-11-20 11:24:53 +0800

    Add the compile options in CMakeLists.txt and config.h.in to generate config.h. :pear: sparrowli

--- main.cc
+++ main.cc
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "config.h"
 
+#ifdef USE_MYMATH
 #include "math/MathFunctions.h"
+#else
+#include <math.h>
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -11,7 +16,14 @@ int main(int argc, char *argv[])
     }
     double base = atof(argv[1]);
     int exponent = atoi(argv[2]);
+
+    #ifdef USE_MYMATH
+    printf("Now we use our own Math library. \n");
     double result = power(base, exponent);
+    #else
+    printf("Now we use our standard library. \n");
+    double result = pow(base, exponent);
+    #endif
     printf("%g ^ %d is %g\n", base, exponent, result);
     return 0;
 }
